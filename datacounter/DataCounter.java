06a6ec5514130069303a95731d1781dcbef306c6
Francesco Vatteroni <39797822+The90Frank@users.noreply.github.com>
2018-11-14 15:28:08 +0100

    Rename datacontainer/DataCounter.java to datacounter/DataCounter.java

--- datacounter/DataCounter.java
+++ datacounter/DataCounter.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
 
 public interface DataCounter<E> extends Iterable<E> {
 	//OVERVIEW: l'interfaccia rappresenta una struttura modificabile di dati omogenei di tipo E organizzati come un Dizionario.
-	//TYPICAL ELEMENT: [K,V] | K è l'elemento di tipo E, V è l'elemento di tipo Integer che è il contatore relativo a quel elemento.
+	//TYPICAL ELEMENT: [K,V] | K Ã¨ l'elemento di tipo E, V Ã¨ l'elemento di tipo Integer che Ã¨ il contatore relativo a quel elemento.
 	
 	public void incCount(E data) throws DataCounterException;
 	//MODIFIES: this
@@ -17,7 +17,7 @@ public interface DataCounter<E> extends Iterable<E> {
 	
 	public int getCount(E data);
 	//EFFECTS: se n==null solleva una NullPointerException (unchecked, presente in Java),
-	//ritorna il contatore relativo all'elemento data, 0 se non è presente
+	//ritorna il contatore relativo all'elemento data, 0 se non Ã¨ presente
 	
 	public Iterator<E> iterator();
 	//EFFECTS: ritorna un iteratore che restituisce gli elementi in ordine casuale
